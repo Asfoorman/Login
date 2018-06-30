77cf60c6fd2fb6da626f152c409ce5e49b7577fe
Anes Mehai <nsamehai@gmail.com>
2018-06-30 00:37:22 -0400

    initial commit

--- Login/ViewController.swift
+++ Login/ViewController.swift
@@ -10,6 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var usernameTextField: UITextField!
+    
+    @IBOutlet weak var forgotUserNameBttn: UIButton!
+    
+    @IBOutlet weak var forgotPasswordBttn: UIButton!
+    
+    /*
+     Forgot User Name Function
+     */
+    
+    @IBAction func forgotUserName(_ sender: Any) {
+        
+        performSegue(withIdentifier: "ForgottenUserNameOrPassword",
+                     sender: forgotUserNameBttn)
+        
+    }
+    
+    
+    @IBAction func forgotPassword(_ sender: Any) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,6 +39,12 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        segue.destination.navigationItem.title = usernameTextField.text
+
     }
 
 
